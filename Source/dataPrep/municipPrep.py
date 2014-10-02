0b41bb7e4616640d23f4ec62614a4292563aa1b0
damiancclarke <damian.c.clarke@gmail.com>
2014-10-01 21:13:29 -0300

    Dump labour data

--- Source/dataPrep/municipPrep.py
+++ Source/dataPrep/municipPrep.py
@@ -3,7 +3,9 @@
 #
 #
 # This file converts various files in a range of xls formats to csv output to i-
-# ncorporate into a data file.  It uses the following data:
+# ncorporate into a data file. Non unicode characters make this a bit of a head-
+# ache.  I use a regex to strip unicodes from imported xls files.  Ths script u-
+# ses the following data:
 #
 #  > Employment data from ENOE
 #  http://www3.inegi.org.mx/sistemas/tabuladosbasicos/tabdirecto.aspx?s=est&c=29188
@@ -21,28 +23,68 @@
 #       siha_2_2_5_10.xls: Escuelas por Municipio (2005-2010)
 #
 #  All data locations and files are listed in section 1
-#
+#  
 #
 #  contact: mailto:damian.clarke@economics.ox.ac.uk
 #
 
 import os
+import sys
 import xlrd
 import csv
+import re
+import codecs
 
 #-------------------------------------------------------------------------------
 #--- (1) Locations on system
 #-------------------------------------------------------------------------------
-labour = '~/investigacion/2014/MexAbort/Data/Labour/Desocupacion2000_2014/'
-municp = '~/investigacion/2014/MexAbort/Data/Municip/'
+base   = '/home/damiancclarke/investigacion/2014/MexAbort/'
+labour = base + 'Data/Labour/Desocupacion2000_2014/'
+municp = base + 'Data/Municip/'
 
-states = ['aguascalientes','baja_california_sur','baja_california','campeche',
-          'chiapas','chihuahua','coahuila_de_zaragoza','colima','distrito_federal',
+states = ['aguascalientes','baja_california','baja_california_sur','campeche',
+          'coahuila_de_zaragoza','colima','chiapas','chihuahua','distrito_federal',
           'durango','guanajuato','guerrero','hidalgo','jalisco','mexico',
           'michoacan_de_ocampo','morelos','nayarit','nuevo_leon','oaxaca','puebla',
           'queretaro','quintana_roo','san_luis_potosi','sinaloa','sonora',
           'tabasco','tamaulipas','tlaxcala','veracruz_de_ignacio_de_la_llave', 
           'yucatan','zacatecas']
+number = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14',
+          '15','16','17','18','19','20','21','22','23','24','25','26','27','28',
+          '29','30','31','32']
 
-for s in states:
-    print s
+#-------------------------------------------------------------------------------
+#--- (2) Dump labour data for each state into csv
+#-------------------------------------------------------------------------------
+fstr = '_tasa_de_desocupacion_total_trimestral.xls'
+expr = '\'/"2000",,,,,,,,/{flag=1;next}/Nota/{flag=0}flag\' '
+
+for n,s in enumerate(states):
+    print 'Converting' + number[n], s + 'to csv'
+    os.system('xls2csv '+labour+s+fstr + '>' + labour+s+'.csv')
+    os.system('awk ' + expr + labour + s +'.csv >' + labour + s +'out.csv')
+    os.remove(labour+s+'.csv')
+    ifile = open(labour+s+'out.csv', 'r')
+    ofile = open(labour+s+'.csv', 'w')
+    year = 2000
+    ofile.write('\"State\",\"Number\",\"year\",\"trimeter\",\"Desocup\",\"dDes\",'+
+                '\"Deseason\",\"dSea\",\"trend\",\"dTre\"'+'\n')
+    for i,line in enumerate(ifile):
+        if i<4:
+            ofile.write('\"' + s + '\",'+'\"' + number[n] + '\",'+'\"' + str(year) 
+                        + '\",'+line)
+        else:
+            if (i-4)%5 == 0:
+                year = year + 1
+                print year
+            else:
+                ofile.write('\"' + s + '\",'+'\"' + number[n] + '\",'+'\"' + 
+                            str(year) + '\",'+line)
+
+    ifile.close()
+    ofile.close()
+    os.remove(labour+s+'out.csv')
+
+#-------------------------------------------------------------------------------
+#--- (3) Dump other municipalty files
+#-------------------------------------------------------------------------------
